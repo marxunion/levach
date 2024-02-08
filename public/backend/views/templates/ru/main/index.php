ac2fbaed0c5e37da4cd5f29b7bfa3a2ef2cf8ece
1 Lorki <gopnik228nk@gmail.com>
2024-02-08 04:12:02 +0200

    Init main page

--- public/backend/views/templates/ru/main/index.php
+++ public/backend/views/templates/ru/main/index.php
@@ -1,1 +1,57 @@
 <?php require "backend/views/components/header_ru.php" ?>
+<div class="wrapper">
+    <div class="article">
+        <div class="article__header">
+
+        </div>
+        <h1 class="article__title"></h1>
+        <div class="article__img"></div>
+        <p class="article__text"></p>
+        <div class="article__buttons">
+            <div class="article__buttons__button"></div>
+            <div class="article__buttons__button"></div>
+        </div>
+        <div class="article__reactions">
+            <div class="article__reactions__rating">
+                <div class="article__reactions__rating__likes"></div>
+                <div class="article__reactions__rating__dislikes"></div>
+            </div>
+        </div>
+    </div>
+    <div class="article">
+        <div class="article__header">
+
+        </div>
+        <h1 class="article__title"></h1>
+        <div class="article__img"></div>
+        <p class="article__text"></p>
+        <div class="article__buttons">
+            <div class="article__buttons__button"></div>
+            <div class="article__buttons__button"></div>
+        </div>
+        <div class="article__reactions">
+            <div class="article__reactions__rating">
+                <div class="article__reactions__rating__likes"></div>
+                <div class="article__reactions__rating__dislikes"></div>
+            </div>
+        </div>
+    </div>
+    <div class="article">
+        <div class="article__header">
+            
+        </div>
+        <h1 class="article__title"></h1>
+        <div class="article__img"></div>
+        <p class="article__text"></p>
+        <div class="article__buttons">
+            <div class="article__buttons__button"></div>
+            <div class="article__buttons__button"></div>
+        </div>
+        <div class="article__reactions">
+            <div class="article__reactions__rating">
+                <div class="article__reactions__rating__likes"></div>
+                <div class="article__reactions__rating__dislikes"></div>
+            </div>
+        </div>
+    </div>
+</div>
--- public/frontend/js_dev/BurgerMenu.js
+++ public/frontend/js_dev/BurgerMenu.js
@@ -1,21 +1,7 @@
 const burger = document.querySelector(".header-burger");
 const menu = document.querySelector(".header-menu");
-const body = document.querySelector("body");
-let unlocker = 1;
+
 burger.addEventListener("click" , () => {
-  if (unlocker == 1) 
-  {
-    console.log(unlocker);
-    body.setAttribute('style', 'padding-right:' + scrollWidth + 'px;overflow: hidden;');
-    unlocker = 0;
-  } 
-  else 
-  {
-    console.log(unlocker);
-    body.setAttribute('style', 'padding-right:0px;overflow: hidden;');
-    unlocker = 1;
-  }
-  
-  menu.classList.toggle("active");
+  //menu.classList.toggle("active");
   burger.classList.toggle("active");
 });
