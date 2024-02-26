2722795749f10b4bc45f57373ca4a50097746424
1 Lorki <gopnik228nk@gmail.com>
2024-02-26 04:25:10 +0200

    DropDown component

--- frontend/src/components/DropDown.vue
+++ frontend/src/components/DropDown.vue
@@ -1,12 +1,60 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-import "./scss/DropDown.scss";
+<script lang="ts">
+import ".scss/DropDown.vue"
 export default {
-    name: "DropDown",
+  props: 
+  {
+    options: 
+    {
+      type: Array,
+      required: true,
+    },
+    default: 
+    {
+      type: String,
+      required: false,
+      default: null,
+    },
+    tabindex: 
+    {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  data() 
+  {
+    return {
+        selected: 
+            this.default
+            ? this.default
+            : this.options.length > 0
+            ? this.options[0]
+            : null,
+        open: false,
+    };
+  },
+  name: "DropDown",
+  mounted() {
+    this.$emit("input", this.selected);
+  },
 };
-
-const dropdown = ref('');
-
 </script>
 
+<template>
+    <div class="custom-select" :tabindex="tabindex" @blur="open = false">
+        <div class="selected" :class="{ open: open }" @click="open = !open">
+            {{ selected }}
+        </div>
+        <div class="items" :class="{ selectHide: !open }">
+            <div
+            v-for="(option, i) of options"
+                :key="i"
+                @click="
+                    selected = option;
+                    open = false;
+                    $emit('input', option);">
+            {{ option }}
+            </div>
+        </div>
+    </div>
+</template>
