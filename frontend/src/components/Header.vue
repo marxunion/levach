bcac0c080a15ab5f08b8961681ae1eab5fcda062
LorkiNikita <lorkiofficial@gmail.com>
2024-03-04 06:50:20 +0200

    Start adding adaptive

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -19,16 +19,16 @@
             <div class="header__bar__search">
                 <input :placeholder="(langData['search'] as string)" type="text" class="header__bar__search__input">
                 <a href="/" class="header__bar__search__button"></a>
-             </div>
+            </div>
             <div class="header__bar__subbar">
                 <a href="#/article/new" class="header__bar__subbar__createarticle">{{ langData['createArticle'] }}</a>
-                <DropDown :options="LangDataHandler.langs" :default="LangDataHandler.currentLanguage.value" class="header__bar__subbar__select" @input="LangDataHandler.changeLanguage" @input-on-mounted="LangDataHandler.changeLanguage"/>
                 <a class="header__bar__subbar__searchmobile">
                     <div class="header__bar__subbar__searchmobile__icon"></div>
                 </a>
                 <a href="#/article/new" class="header__bar__subbar__createarticlemobile">
                     <div class="header__bar__subbar__createarticlemobile__icon"></div>
                 </a>
+                <DropDown :options="LangDataHandler.langs" :default="LangDataHandler.currentLanguage.value" class="header__bar__subbar__select" @input="LangDataHandler.changeLanguage" @input-on-mounted="LangDataHandler.changeLanguage"/>
             </div>
         </div>
         <div class="header__burger header-burger">
