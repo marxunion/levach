c4ebbe303b863f8dd0c13bfe71724300272c0091
LorkiNikita <lorkiofficial@gmail.com>
2024-04-09 08:25:39 +0300

    Added favicon.ico and fix log bug

--- backend/index.php
+++ backend/index.php
@@ -27,9 +27,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 Settings::Init();
 $logger = Logger::initInstance('main');
-$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/main.log', Level::Debug));
-$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/warnings.log', Level::Warning));
-$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/errors.log', Level::Error));
+$logger->pushHandler(new StreamHandler(__DIR__.'/logs/main.log', Level::Debug));
+$logger->pushHandler(new StreamHandler(__DIR__.'/logs/warnings.log', Level::Warning));
+$logger->pushHandler(new StreamHandler(__DIR__.'/logs/errors.log', Level::Error));
 Database::Init();
 
 $app = AppFactory::create();
