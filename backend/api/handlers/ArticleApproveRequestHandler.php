29512363bf27a9b697959c1ead6082bc6df256ed
LorkiNikita <lorkiofficial@gmail.com>
2024-05-05 09:51:55 +0300

    Update

--- backend/api/handlers/ArticleApproveRequestHandler.php
+++ backend/api/handlers/ArticleApproveRequestHandler.php
@@ -11,18 +11,63 @@ class ArticleApproveRequestHandler extends BaseHandlerRouteWithArgs
 {
     public function Init()
     {
-        if($this->args['editCode'])
+        $parsedBody = $this->request->getParsedBody();
+        if(is_array($parsedBody))
         {
+            $this->parsedBody = $parsedBody;
             
+            if(isset($this->parsedBody['csrfToken']))
+            {
+                if(csrfTokenHandler::checkCsrfToken($this->parsedBody['csrfToken']))
+                {
+                    if(isset($this->args['editCode']))
+                    {
+                        $this->model = new ArticleApproveRequestModel();
+                    }
+                    else
+                    {
+                        throw new Error(404, "Article for edit not found", "Article for edit not found");
+                    }
+                }
+                else
+                {
+                    throw new Error(403, "Invalid CSRF token", "Invalid CSRF token");
+                }
+            }
+            else
+            {
+                throw new Error(403, "Invalid CSRF token", "Invalid CSRF token");
+            }
         }
         else
         {
-
+            throw new Error(403, "Invalid CSRF token", "Invalid CSRF token");
         }
     }
 
     public function Process()
     {
+        $articleId = $this->model->getArticleIdByEditCode($this->args['editCode']);
 
+        if($articleId)
+        {
+            $ratingToRequestApprove = AdminSettingsGetHandler::getSetting("article_need_rating_to_approve_editorially");
+            if(isset($ratingToRequestApprove))
+            {
+                if(> $ratingToRequestApprove)
+                {
+                    $this->model->requestApprove($articleId);
+                }
+                else
+                {
+
+                }
+            }
+            
+        }
+        else
+        {
+            throw new Error(404, "Article for edit not found", "Article for edit not found");
+        }
     }
 }
