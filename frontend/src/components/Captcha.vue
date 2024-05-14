7c43845f6dcb014e0a29b13b1a0e3a8dec406970
LorkiNikita <lorkiofficial@gmail.com>
2024-05-14 08:22:21 +0300

    Update

--- frontend/src/components/Captcha.vue
+++ frontend/src/components/Captcha.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-    import { ref, Ref, ComputedRef, defineEmits } from "vue";
+    import { ref, Ref, ComputedRef, defineEmits, onUnmounted } from "vue";
 
     import { VueRecaptcha } from 'vue-recaptcha';
 
     import { JsonData } from "../ts/interfaces/JsonData";
 
     import { LangDataHandler } from "../ts/handlers/LangDataHandler";
-    import langsData from "./modals/locales/InfoModal.json";
+    import langsData from "./locales/Captcha.json";
     
+    const langData : ComputedRef<JsonData> = LangDataHandler.initLangDataHandler("Captcha", langsData).langData;
+
     const emits = defineEmits(["onVerify", "onExpired", "onError"]);
 
     const recaptchaSitekey = ref('6LfaS9spAAAAAFo0_FElcn_sYvxjb3wlt8czk2E2');
@@ -28,21 +30,36 @@
         emits('onError');
     };
 
-
     const executeRecaptcha = () => 
     {
         const recaptchaInstance = recaptcha.value?.execute();
     };
     
-    const langData : ComputedRef<JsonData> = LangDataHandler.initLangDataHandler("InfoModal", langsData).langData;
+    // 
+    onUnmounted(() =>
+    {
+        const element = document.querySelector('[style="visibility: hidden; position: absolute; width: 100%; top: -10000px; left: 0px; right: 0px; transition: visibility 0s linear 0.3s, opacity 0.3s linear; opacity: 0;"]');
+
+        if(element) 
+        {
+            const parent = element.parentElement;
+
+            if(parent)
+            {
+                parent.removeChild(element);
+            }
+        }
+    });
+
+    
 </script>
 
 <template>
     <div class="captcha">
         <p class="captcha__text">
-            This site is protected by reCAPTCHA and the Google
-            <a class="captcha__text__link" target="_blank" href="https://policies.google.com/privacy">Privacy Policy</a> and
-            <a class="captcha__text__link" target="_blank" href="https://policies.google.com/terms">Terms of Service</a> apply.
+            {{ langData['captchaText'] }}
+            <a class="captcha__text__link" target="_blank" href="https://policies.google.com/privacy">{{ langData['captchaTextLinkPrivacyPolicy'] }}</a>{{ langData['captchaText1'] }}
+            <a class="captcha__text__link" target="_blank" href="https://policies.google.com/terms">{{ langData['captchaTextLinkPrivacyTermsOfService'] }}</a>{{ langData['captchaText2'] }}
         </p>
         
         <div class="captcha__handler">
@@ -56,7 +73,6 @@
             </vue-recaptcha>
         </div>
     </div>
-   
 </template>
 
 <style lang="scss" scoped src="./scss/Captcha.scss"></style>
