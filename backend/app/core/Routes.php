341ae6f2e082e054d67f68482b24f367633a4216
LorkiNikita <lorkiofficial@gmail.com>
2024-05-22 02:35:39 +0300

    Update

--- backend/app/core/Routes.php
+++ backend/app/core/Routes.php
@@ -29,7 +29,7 @@ class Routes
         {
             self::$app->get('/frontend/assets/{file:.*}', function (Request $request, Response $response, array $args) 
             {
-                $file = __DIR__ . '/../frontend/dist/' . $args['file'];
+                $file = __DIR__ . '/../../frontend/dist/' . $args['file'];
 
                 if(file_exists($file)) 
                 {
@@ -43,7 +43,7 @@ class Routes
 
             $app->get('/', function (Request $request, Response $response) 
             {
-                $file = __DIR__ . '/../frontend/index.html';
+                $file = __DIR__ . '/../../frontend/index.html';
                 echo($file);
                 if(file_exists($file)) 
                 {
