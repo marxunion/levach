7a5463b563c63ddb76e64e811316eb58cb1d31b1
LorkiNikita <lorkiofficial@gmail.com>
2024-05-22 07:45:07 +0300

    Update

--- backend/api/handlers/ArticleCommentNewHandler.php
+++ backend/api/handlers/ArticleCommentNewHandler.php
@@ -23,40 +23,33 @@ class ArticleCommentNewHandler extends BaseHandlerRouteWithArgs
                     {
                         if(CSRFTokenHandler::checkCsrfToken($this->parsedBody['csrfToken']))
                         {
-                            if(AdminStatusHandler::isAdmin($this->request->getCookieParams()))
+                            if(!empty($this->args['viewCode']))
                             {
-                                if(!empty($this->args['viewCode']))
+                                if(!empty($this->parsedBody['text']))
                                 {
-                                    if(!empty($this->parsedBody['text']))
+                                    $this->parsedBody['ratingInfluence'] = 0;
+                                    if(isset($this->parsedBody['rating_influence']))
                                     {
-                                        $this->parsedBody['ratingInfluence'] = 0;
-                                        if(isset($this->parsedBody['rating_influence']))
+                                        if($this->parsedBody['rating_influence'] == 1)
                                         {
-                                            if($this->parsedBody['rating_influence'] == 1)
-                                            {
-                                                $this->parsedBody['ratingInfluence'] = 1;
-                                            }
-                                            else if($this->parsedBody['rating_influence'] == 2)
-                                            {
-                                                $this->parsedBody['ratingInfluence'] = -1;
-                                            }
+                                            $this->parsedBody['ratingInfluence'] = 1;
                                         }
+                                        else if($this->parsedBody['rating_influence'] == 2)
+                                        {
+                                            $this->parsedBody['ratingInfluence'] = -1;
+                                        }
+                                    }
                                         
-                                        $this->model = new ArticleCommentNewModel();
-                                    }
-                                    else
-                                    {
-                                        throw new Error(400, "Invalid comment text", "Invalid article text");
-                                    }
+                                    $this->model = new ArticleCommentNewModel();
                                 }
                                 else
                                 {
-                                    throw new Error(400, "Invalid article viewCode", "Invalid article viewCode");
+                                    throw new Error(400, "Invalid comment text", "Invalid article text");
                                 }
                             }
                             else
                             {
-                                throw new Error(403, "Invalid admin token", "Invalid admin token");
+                                throw new Error(400, "Invalid article viewCode", "Invalid article viewCode");
                             }
                         }
                         else
