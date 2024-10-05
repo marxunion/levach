0ec0e7f516d24390b74b277cd5e5932eb2478ab3
LorkiVG <lorkiofficial@gmail.com>
2024-10-06 01:52:44 +0300

    Update

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -64,11 +64,12 @@
                 <input @keyup.enter="onSearchButton" v-model="searchText" :placeholder="(langData['search'] as string)" type="text" class="header__bar__search__input">
                 <a @click="onSearchButton" class="header__bar__search__button"></a>
             </div>
-            <label class="header__bar__switch">
-                <input type="checkbox" @change="onThemeSwitchClick" :checked="ThemeHandler.instance.getCurrentTheme.value == 'light' ? false : true" id="theme__switcher" class="header__bar__switch__checkbox">
-                <span class="header__bar__switch__round"></span>
-            </label>
+            
             <div class="header__bar__subbar">
+                <label class="header__bar__subbar__switch">
+                    <input type="checkbox" @change="onThemeSwitchClick" :checked="ThemeHandler.instance.getCurrentTheme.value == 'light' ? false : true" id="theme__switcher" class="header__bar__subbar__switch__checkbox">
+                    <span class="header__bar__subbar__switch__round"></span>
+                </label>
                 <a href="#/article/new" class="header__bar__subbar__createarticle">{{ langData['createArticle'] }}</a>
                 <DropDown :options="LangDataHandler.langs" :default="LangDataHandler.currentLanguage.value" class="header__bar__subbar__select" @input="LangDataHandler.changeLanguage" @input-on-mounted="LangDataHandler.changeLanguage"/>
                 <p class="header__bar__subbar__searchmobile" @click="openModal(SearchMobileModal)">
@@ -77,7 +78,6 @@
                 <a href="#/article/new" class="header__bar__subbar__createarticlemobile">
                     <img src="../assets/img/header/createarticle.svg" alt="Create Article" class="header__bar__subbar__createarticlemobile__icon">
                 </a>
-                
             </div>
         </div>
         <div class="header__burger header-burger" @click="$emit('toggleBurger')">
