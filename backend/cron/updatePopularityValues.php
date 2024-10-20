85d5e6d5a7dacd64ae5acf7ef08d3e8b566ffea3
LorkiVG <lorkiofficial@gmail.com>
2024-10-21 02:57:24 +0300

    Refactoring

--- backend/cron/updatePopularityValues.php
+++ backend/cron/updatePopularityValues.php
@@ -14,9 +14,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 Settings::Init();
 $logger = Logger::initInstance('main');
-$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/main.log', Level::Debug));
-$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/warnings.log', Level::Warning));
-$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/errors.log', Level::Error));
+$logger->initExceptionHandlers();
+
 Database::Init();
 
 AdminArticlesUpdatePopularitySortHandler::_updatePopularityValues();
